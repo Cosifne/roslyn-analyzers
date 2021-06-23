d972f1ad9cc366aeabc97460acf47f3d46e3e294
Shen Chen <cos_ifne@hotmail.com>
2021-06-22 17:37:51 -0700

    Add test & all wrapper logic

--- src/NetAnalyzers/Core/Microsoft.CodeQuality.Analyzers/QualityGuidelines/AvoidMultipleEnumerations.cs
+++ src/NetAnalyzers/Core/Microsoft.CodeQuality.Analyzers/QualityGuidelines/AvoidMultipleEnumerations.cs
@@ -1,21 +1,73 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Microsoft.CodeQuality.Analyzers.QualityGuidelines
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
     public sealed class AvoidMultipleEnumerations : DiagnosticAnalyzer
     {
+        /// <summary>
+        /// TODO: How to decide a good rule id.
+        /// </summary>
+        private const string RuleId = "HAA1838";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidMultipleEnumerationsTitle), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+        private static readonly LocalizableString s_description = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidMultipleEnumerationsMessage), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+
+        internal static readonly DiagnosticDescriptor MultipleEnumerableDescriptor = DiagnosticDescriptorHelper.Create(
+            RuleId,
+            s_localizableTitle,
+            s_description,
+            DiagnosticCategory.Performance,
+            RuleLevel.Disabled,
+            description: null,
+            isPortedFxCopRule: false,
+            isDataflowRule: true);
+
+        private static readonly ImmutableArray<string> s_wellKnownNamesNeedAnalyze = ImmutableArray
+            .Create(WellKnownTypeNames.SystemCollectionsGenericIEnumerable1, WellKnownTypeNames.SystemCollectionsIEnumerable);
+
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-            => ImmutableArray<DiagnosticDescriptor>.Empty;
+            => ImmutableArray<DiagnosticDescriptor>.Empty.Add(MultipleEnumerableDescriptor);
 
         public override void Initialize(AnalysisContext context)
         {
-            throw new NotImplementedException();
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterCompilationStartAction(CompliationStartAction);
+        }
+
+        private static void CompliationStartAction(CompilationStartAnalysisContext context)
+        {
+            context.RegisterOperationBlockAction(OperationAction);
+        }
+
+        private static void OperationAction(OperationBlockAnalysisContext context)
+        {
+            var owningSymbol = context.OwningSymbol;
+            var cancellationToken = context.CancellationToken;
+            var typeSymbolsNeedAnalyze = s_wellKnownNamesNeedAnalyze.Select(name => context.Compilation.GetOrCreateTypeByMetadataName(name)).ToImmutableArray();
+
+            foreach (var operation in context.OperationBlocks)
+            {
+                if (ShouldAnalyze(operation))
+                {
+                    var cfg = context.GetControlFlowGraph(operation);
+                }
+            }
+
+            // TODO : Write the condition
+            bool ShouldAnalyze(IOperation operation)
+            {
+                return true;
+            }
         }
     }
 }
---
+++ src/NetAnalyzers/UnitTests/Microsoft.CodeQuality.Analyzers/QualityGuidelines/AvoidMultipleEnumerationsTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeQuality.Analyzers.QualityGuidelines;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.QualityGuidelines.AvoidMultipleEnumerations,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.QualityGuidelines.AvoidMultipleEnumerations,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeAnalysis.NetAnalyzers.UnitTests.Microsoft.CodeQuality.Analyzers.QualityGuidelines
+{
+    public class AvoidMultipleEnumerationsTests
+    {
+        [Fact]
+        public Task Test()
+        {
+            var code = @"
+public class Bar
+{
+    public void Sub()
+    {
+        IEnumerable<int> i = Enumerable.Range(1, 10);
+        {|#1:i|}|.Take(10);
+        {|#2:i|}.Take(10);
+    }
+}";
+            return VerifyCS.VerifyAnalyzerAsync(
+                code,
+                VerifyCS.Diagnostic(AvoidMultipleEnumerations.MultipleEnumerableDescriptor).WithLocation(1),
+                VerifyCS.Diagnostic(AvoidMultipleEnumerations.MultipleEnumerableDescriptor).WithLocation(2));
+        }
+    }
+}
